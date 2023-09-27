534d0f3d89c4fae6311f60738f9cae859773acfa
SUNIL7978 <Sunilprusty16@gmail.com>
2023-09-27 16:55:39 +0530

    This is the change for title

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,7 +5,7 @@ import SectionTitle from './SectionTitle';
 const Projects = () => {
   return (
     <section className="py-20 aligns-item" id="projects">
-      <SectionTitle text="Web Devolper" />
+      <SectionTitle text="Web Devoloper" />
       <div className="grid items-center py-16 gap-8 lg:grid-cols-2 xl:grid-cols-3">
         {projects.map((project) => {
           return <ProjectCard key={project.id} {...project} />;
